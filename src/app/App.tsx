3c5805f2e585525863860eed1eaa69b58e46d774
kobzarenko-volodymyr <kobzarenkovolodymyr@gmail.com>
2023-02-07 10:04:51 +0200

    created a 'button' component and added a file loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,11 +5,10 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { Navbar } from "widgets/Navbar";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
-    <div className={classNames("app", { hovered: true }, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
+    <div className={classNames("app", {}, [theme])}>
       <Navbar />
       <AppRouter />
     </div>
