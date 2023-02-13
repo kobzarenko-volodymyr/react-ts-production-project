09e54c37a1880145647078fd3caee8330e1747f8
kobzarenko-volodymyr <kobzarenkovolodymyr@gmail.com>
2023-02-13 15:13:51 +0200

    added React Testing Library and tests for Button and Sidebar

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,9 +1,17 @@
+import path from "path";
+
 export default {
   clearMocks: true,
   testEnvironment: "jsdom",
   moduleDirectories: ["node_modules"],
+  modulePaths: ["<rootDir>src"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
   rootDir: "../..",
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.s?css$": "identity-obj-proxy",
+    "\\.svg": path.resolve(__dirname, "jestEmptyComponent.tsx"),
+  },
   testMatch: ["<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)"],
   coveragePathIgnorePatterns: ["/node_modules/"],
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -18,11 +18,14 @@ export const Sidebar: React.FC<SidebarProps> = (props) => {
 
   return (
     <div
+      data-testid="sidebar"
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
         className,
       ])}
     >
-      <button onClick={onToggle}>toogle</button>
+      <button data-testid="sidebar-toggle" onClick={onToggle}>
+        toogle
+      </button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
