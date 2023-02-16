4ed5cd534a5f42d5ee4e75eda033d539880a62de
kobzarenko-volodymyr <kobzarenkovolodymyr@gmail.com>
2023-02-16 14:49:26 +0200

    changed sidebar

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,14 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import { Button, ThemeButton } from "shared/ui/Button/Button";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
 
 describe("Button", () => {
   test("Test render", () => {
+    // render чтобы изолированно отрендерить компонент
     render(<Button>TEST</Button>);
     expect(screen.getByText("TEST")).toBeInTheDocument();
   });
 
   test("Test clear theme", () => {
-    render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+    render(<Button theme={ButtonTheme.CLEAR}>TEST</Button>);
     expect(screen.getByText("TEST")).toHaveClass("clear");
     screen.debug();
   });
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { classNames } from "shared/lib/classNames/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -9,18 +8,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={"/"}
-          className={cls.mainLink}
-        >
-          Main page
-        </AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
-          About page
-        </AppLink>
-      </div>
+      <div className={cls.links}>/</div>
     </div>
   );
 };
